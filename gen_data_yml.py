a7abba04fad29442ee92555bbe68d8440f5db373
kur <rhdnfka94@gmail.com>
2019-02-24 16:58:47 +0900

    Add class(origin)_map generation

--- gen_data_yml.py
+++ gen_data_yml.py
@@ -12,3 +12,16 @@ label_dirpaths = list(map(lambda name: os.path.join(labels_directory,name), labe
 label_paths_seq = map(lambda ldir: [filename_ext(ldir).name,file_paths(ldir)], label_dirpaths)
 label_paths_dic = {dirname:human_sorted(label_paths) for dirname,label_paths in label_paths_seq} 
 
+if dataset_name == 'boundary_bioseg':
+    origin_map = {(0.0, 0.0, 1.0): [1.0, 1.0, 1.0], 
+                  (0.0, 1.0, 0.0): [0.0, 1.0, 0.0], 
+                  (1.0, 0.0, 0.0): [0.0, 0.0, 0.0]}
+else:
+    #TODO: incomplete.
+    origin_maps = []
+    #for label_dirpath in label_dirpaths:
+    for label_dirname in label_dirnames:
+        for label_path in label_paths_dic[label_dirname]:
+            label = cv2.imread(label_path)
+            print(categorize(label)[1])
+
