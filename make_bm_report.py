a7b309d8dc9b6c2ec5a915e4e41630e36c67f25f
KUR-creative <rhdnfka94@gmail.com>
2019-03-15 21:17:01 +0900

    Write imgpaths and train/valid key row

--- make_bm_report.py
+++ make_bm_report.py
@@ -1,7 +1,21 @@
+import os,re,yaml
 import xlsxwriter
-from utils import file_paths, human_sorted, filename
-import os
-import yaml
+from collections import namedtuple
+def filename_ext(path):
+    name_ext = namedtuple('name_ext','name ext')
+    return name_ext( *os.path.splitext(os.path.basename(path)) )
+
+def filename(path):
+    return filename_ext(path).name
+
+def extension(path):
+    return filename_ext(path).ext
+
+def human_sorted(iterable):
+    ''' Sorts the given iterable in the way that is expected. '''
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(iterable, key = alphanum_key)
 
 root = '.'
 is_result = lambda p:('[b' in p) or ('[m' in p)
@@ -62,22 +76,29 @@ expr_info_vals_col.append(expr['BATCH_SIZE'])   # batch size
 expr_info_vals_col.append(expr['NUM_EPOCHS'])   # number of epochs
 expr_info_vals_col.append(expr['STEPS_PER_EPOCH']) # steps/epoch
 
-expr_sh.write_column(0,0, expr_info_keys_col)
-expr_sh.write_column(0,1, expr_info_vals_col) # y,x
 key_format = workbook.add_format({'bold':True})
-expr_sh.set_column(0,0, cell_format=key_format)
+expr_sh.write_column(0,0, expr_info_keys_col, key_format)
+expr_sh.write_column(0,1, expr_info_vals_col) # y,x
 
-score_keys_row = ['train', 'f1 score', 'dice_obj']
-score_keys_y = len(expr_info_keys_col) + 1
-score_beg_y = score_keys_y + 1
-expr_sh.write_row(score_keys_y,0, score_keys_row)
-expr_sh.set_row(score_keys_y,0, cell_format=key_format)
+train_keys_row = ['train', 'f1 score', 'dice_obj']
+train_keys_y = len(expr_info_keys_col) + 1
+expr_sh.write_row(train_keys_y,0, train_keys_row, key_format)
 
 result_fname = '[result]' + name + '.yml'
 result_fpath = os.path.join(name,result_fname)
 with open(result_fpath) as result_file:
     result = yaml.load(result_file)
-print(result)
+train_names = [filename(p) for p in result['train_imgs']]
+valid_names = [filename(p) for p in result['valid_imgs']]
+
+valid_keys_row = ['valid', 'f1 score', 'dice_obj']
+train_beg_y = train_keys_y + 1
+valid_keys_y = train_beg_y + len(train_names) + 2 
+expr_sh.write_row(valid_keys_y,0, valid_keys_row, key_format)
+valid_beg_y = valid_keys_y + 1
+
+expr_sh.write_column(train_beg_y, 0, train_names) 
+expr_sh.write_column(valid_beg_y, 0, valid_names) 
 '''
 for fname in result_dirpaths:
     valid_fname = fname.replace('[','__').replace(']','__')
