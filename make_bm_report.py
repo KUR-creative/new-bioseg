08ab2cacc68d2c75631e1228171cfe2b0112f0fc
KUR-creative <rhdnfka94@gmail.com>
2019-03-15 21:38:41 +0900

    Rearrange codes

--- make_bm_report.py
+++ make_bm_report.py
@@ -21,7 +21,6 @@ root = '.'
 is_result = lambda p:('[b' in p) or ('[m' in p)
 result_dirpaths = human_sorted(filter(is_result, os.listdir(root)))
 print(result_dirpaths,sep='\n')
-#print(result_dirpaths)
 
 expr_info_keys_col = [
     'expr name', 
@@ -40,17 +39,22 @@ expr_info_keys_col = [
 
 workbook = xlsxwriter.Workbook('test.xlsx')
 #summay_sh = workbook.add_worksheet('summary')
+key_format = workbook.add_format({'bold':True})
 
 name = result_dirpaths[0]
 valid_fname = name.replace('[','__').replace(']','__')
 expr_sh = workbook.add_worksheet(valid_fname)
 
+# Open data ymls
 config_fname = '[config]' + name + '.yml'
 config_fpath = os.path.join(name,config_fname)
-with open(config_fpath) as expr_file:
+result_fname = '[result]' + name + '.yml'
+result_fpath = os.path.join(name,result_fname)
+with open(config_fpath) as expr_file, open(result_fpath) as result_file:
     expr = yaml.load(expr_file)
-#print(expr)
+    result = yaml.load(result_file)
 
+# Write experiment info
 expr_info_vals_col = []
 expr_info_vals_col.append(name)                 # expr name
 dataset_name = filename(expr['DATASET_YML'])
@@ -76,29 +80,31 @@ expr_info_vals_col.append(expr['BATCH_SIZE'])   # batch size
 expr_info_vals_col.append(expr['NUM_EPOCHS'])   # number of epochs
 expr_info_vals_col.append(expr['STEPS_PER_EPOCH']) # steps/epoch
 
-key_format = workbook.add_format({'bold':True})
 expr_sh.write_column(0,0, expr_info_keys_col, key_format)
 expr_sh.write_column(0,1, expr_info_vals_col) # y,x
 
+# Write f1 scores and dice_objs
+# Write train/valid key row, and avrg key cell
 train_keys_row = ['train', 'f1 score', 'dice_obj']
-train_keys_y = len(expr_info_keys_col) + 1
-expr_sh.write_row(train_keys_y,0, train_keys_row, key_format)
+valid_keys_row = ['valid', 'f1 score', 'dice_obj']
 
-result_fname = '[result]' + name + '.yml'
-result_fpath = os.path.join(name,result_fname)
-with open(result_fpath) as result_file:
-    result = yaml.load(result_file)
+train_keys_y = len(expr_info_keys_col) + 1
+train_beg_y = train_keys_y + 1
+train_avrg_y = train_beg_y + len(result['train_imgs']) 
+valid_keys_y = train_avrg_y + 2 
+valid_beg_y = valid_keys_y + 1
+valid_avrg_y = valid_beg_y + len(result['valid_imgs']) 
+
+expr_sh.write_row(train_keys_y,0, train_keys_row, key_format)
+#expr_sh.write(train_avrg_y,0, 'train avrg', key_format)
+expr_sh.write_row(valid_keys_y,0, valid_keys_row, key_format)
+#expr_sh.write(valid_avrg_y,0, 'valid avrg', key_format)
+
+# Write data columns
 train_names = [filename(p) for p in result['train_imgs']]
 valid_names = [filename(p) for p in result['valid_imgs']]
-
-valid_keys_row = ['valid', 'f1 score', 'dice_obj']
-train_beg_y = train_keys_y + 1
-valid_keys_y = train_beg_y + len(train_names) + 2 
-expr_sh.write_row(valid_keys_y,0, valid_keys_row, key_format)
-valid_beg_y = valid_keys_y + 1
-
-expr_sh.write_column(train_beg_y, 0, train_names) 
-expr_sh.write_column(valid_beg_y, 0, valid_names) 
+expr_sh.write_column(train_beg_y,0, train_names) 
+expr_sh.write_column(valid_beg_y,0, valid_names) 
 '''
 for fname in result_dirpaths:
     valid_fname = fname.replace('[','__').replace(']','__')
