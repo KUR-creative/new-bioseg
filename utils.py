0c6baff67d6e41ff73c1911c6b1f917f32da374c
kur <rhdnfka94@gmail.com>
2019-07-05 14:23:16 +0900

    Fix map_colors in #class=4 (added map_pixels)

--- utils.py
+++ utils.py
@@ -82,33 +82,58 @@ def load_imgs(img_paths, mode_flag=cv2.IMREAD_COLOR):
     #return map(lambda path: bgr_float32(cv2.imread(path, mode_flag)), img_paths) 
     return map(lambda path: bgr_float32(imread(path, mode_flag)), img_paths) 
 
-def map_colors(img, dst_src_colormap): # {dst1:src1, dst2:src2, ...}
+def map_pixels(img, cond_color, true_color, false_color=None):
+    h,w,c = img.shape
+    cond_color  = [[cond_color]]
+    true_color  = [[true_color]]
+    false_color = (np.zeros_like(true_color) 
+                   if false_color is None 
+                   else false_color)
+    dst_c = false_color.shape[-1]
+    t_pixel = np.ones_like(cond_color)
+    f_pixel = np.zeros_like(cond_color)
+    return F.rcompose(
+        # make mask
+        lambda c: np.repeat(c, h*w, axis=0), 
+        lambda m: m.reshape([h,w,c]),
+        # make where array 
+        lambda m: np.all(img == m, axis=-1),
+        lambda w: np.expand_dims(w, axis=-1),
+        lambda w: np.repeat(w, 3, axis = -1),
+        # make t/f map
+        lambda w: np.where(w, t_pixel, f_pixel),
+        lambda m: np.expand_dims(m[:,:,0], axis=-1),
+        lambda m: np.repeat(m, dst_c, axis=-1),
+        # make return value
+        lambda r: r * true_color,
+        lambda r: r.astype(np.uint8),
+    )(cond_color)
+
+def map_colors(img, dst_src_colormap): 
+    # {dst1:src1, dst2:src2, ...}
+    # {one-hot: bgr, ...}
     h,w,_ = img.shape
     n_classes = len(dst_src_colormap)
     ret_img = np.zeros((h,w,n_classes))
 
     if n_classes == 2:
         img_b, img_g, img_r = np.rollaxis(img, axis=-1)
-        for i,(dst_color,(src_b,src_g,src_r)) in enumerate(dst_src_colormap.items()):
+        for dst_color, (src_b,src_g,src_r) in dst_src_colormap.items():
             masks = ((img_b == src_b) 
                    & (img_g == src_g) 
                    & (img_r == src_r)) # if [0,0,0]
             ret_img[masks] = dst_color
     elif n_classes == 3:
         img_b, img_g, img_r = np.rollaxis(img, axis=-1)
-        for i,(dst_color,(src_b,src_g,src_r)) in enumerate(dst_src_colormap.items()):
+        for dst_color, (src_b,src_g,src_r) in dst_src_colormap.items():
             masks = ((img_b == src_b) 
                    & (img_g == src_g) 
                    & (img_r == src_r)) # if [0,0,0]
             ret_img[masks] = dst_color
     elif n_classes == 4:
-        img_b, img_g, img_r, img_3 = np.rollaxis(img, axis=-1)
-        for i,(dst_color,(src_b,src_g,src_r,src_3)) in enumerate(dst_src_colormap.items()):
-            masks = ((img_b == src_b) 
-                   & (img_g == src_g) 
-                   & (img_r == src_r)
-                   & (img_3 == src_3)) # if [0,0,0]
-            ret_img[masks] = dst_color
+        for c,(dst_color, src_bgr) in enumerate(dst_src_colormap.items()):
+            ret_img[:,:,c] = map_pixels(img, src_bgr, dst_color)[:,:,c]
+
     # ... TODO: refactor it!!!
     return ret_img
 
@@ -116,6 +141,7 @@ from keras.utils import to_categorical
 def categorize_with(img, origin_map):
     colors = np.unique(img.reshape(-1,img.shape[2]), axis=0)
     #print(colors, origin_map)
+    #exit()
     assert set(map(tuple, colors.tolist() )) <= set(map(tuple, origin_map.values() )),\
         " %s > %s " % ( str(set(map(tuple, colors.tolist()))),
                         str(set(map(tuple, origin_map.values()))) )
